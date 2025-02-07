025736999b540784fc3d552b79da030c049b2e7a
Marie <mmdevq@gmail.com>
2025-02-07 10:26:15 -0800

    create form validation

--- NetCoreStore/Models/Category.cs
+++ NetCoreStore/Models/Category.cs
@@ -8,9 +8,11 @@ namespace NetCoreStore.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(25)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,50,ErrorMessage = "Display order must be between 1-50")]
         public int DisplayOrder { get; set; }
     }
 }
